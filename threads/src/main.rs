e71a7b36c969c73151289a3cb661c85d430da900
Rob Williams <rwilliams@spotify.com>
2021-01-24 11:00:14 -0500

    added channels example

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,5 +1,6 @@
 use std::thread;
 use std::time::Duration;
+use std::sync::mpsc;
 
 fn main() {
     // Thread basics: spawn, move, and join. https://doc.rust-lang.org/book/ch16-01-threads.html
@@ -20,5 +21,35 @@ fn main() {
     println!("after join");
 
     // Channels and message passing. https://doc.rust-lang.org/book/ch16-02-message-passing.html
+    let (tx, rx) = mpsc::channel();
+    let tx1 = mpsc::Sender::clone(&tx);
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi"),
+            String::from("from"),
+            String::from("the"),
+            String::from("thread"),
+        ];
+        for val in vals {
+            tx.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
     
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("hi2"),
+            String::from("from2"),
+            String::from("the2"),
+            String::from("thread2"),
+        ];
+        for val in vals {
+            tx1.send(val).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for received in rx {
+        println!("Received: {}", received);
+    }
 }
